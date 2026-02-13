5abf7c85db1727d7b3ad031e94ebec658e20517c
chiya0123 <minji20031202@gmail.com>
2026-02-13 11:14:46 +0900

    [Silver V] Title: 카드1, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class Queue:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        self.cnt = 0
+    def push(self,data):
+        new_node=Node(data)
+        if self.head==None:
+            self.tail=self.head = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node   
+        self.cnt += 1
+
+    def pop(self):
+        if self.empty():
+            return -1
+        data=self.head.data
+        self.head=self.head.next
+        self.cnt -= 1
+        if self.empty():
+            self.tail=None
+        return data
+        
+    def size(self):
+        return self.cnt
+
+    def empty(self):
+        return self.head is None
+
+    def front(self):
+        if self.empty():
+            return -1
+        return self.head.data
+
+    def back(self):
+        return -1 if self.empty() else self.tail.data
+
+card=Queue()
+n=int(input())
+
+for i in range(1,n+1):
+    card.push(i)
+while card.size() != 1:
+    print(card.pop(),end=' ')
+    card.push(card.pop())
+print(card.front())
